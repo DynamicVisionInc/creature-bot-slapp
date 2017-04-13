800c82ad0fc2c3626b9b794616fa077a91ed2f09
JaronKing <jaronjking@gmail.com>
2017-04-13 14:58:26 -0700

    V2 enhancement.

--- library/oauth.js
+++ library/oauth.js
@@ -35,7 +35,7 @@ module.exports = (server, db) => {
     res.send(`
     <html>
     <body style='display: flex; align-items: center; justify-content: center; flex-direction: column;font-family: sans-serif'>
-      <h1><a href='https://github.com/BeepBoopHQ/slapp'>Slapp</a> Firebase Slack App</h1>
+      <h1><a href='https://github.com/BeepBoopHQ/slapp'>Slapp v2</a> Firebase Slack App</h1>
       <a href="https://slack.com/oauth/authorize?scope=bot&client_id=${clientId}&redirect_uri=${redirectURI}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
       <a href="https://github.com/selfcontained/slapp-firebase-example"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
     </body>
