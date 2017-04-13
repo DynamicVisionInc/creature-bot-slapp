425c03b17c6ace03d4438394862809e1c261d8ed
JaronKing <jaronjking@gmail.com>
2017-04-13 09:46:16 -0700

    Changes quptes used in env.sh.

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@ export SLACK_VERIFY_TOKEN="hQE0ZY9meqlhwznwjgHeNI4J"
 export SLACK_CLIENT_ID="2918553111.167195077991"
 export SLACK_CLIENT_SECRET="78d69387b95c71dde707ffc9059460e2"
 export FIREBASE_DB_URL="https://creature-9ef38.firebaseio.com"
-export FIREBASE_SERVICE_ACCOUNT_BASE64="base64 service-account.json"
+export FIREBASE_SERVICE_ACCOUNT_BASE64=`base64 service-account.json`
