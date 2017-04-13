018ce5f765e95a725c2684bf66d5e47950681d95
JaronKing <jaronjking@gmail.com>
2017-04-13 08:47:29 -0700

    Updated to use Firebase and Firebase admin, working locally, needs to be tested remotely.

---
+++ library/oauth.js
@@ -0,0 +1,86 @@
+'use strict'
+
+const bodyParser = require('body-parser')
+const slack = require('slapp/client')
+
+const ConvoStore = require('./convo-store')
+
+module.exports = (server, db) => {
+  let clientId = process.env.SLACK_CLIENT_ID
+  let clientSecret = process.env.SLACK_CLIENT_SECRET
+  let domain = process.env.DOMAIN
+
+  server.get('/add', (req, res) => {
+    console.log('add')
+
+    db.saveConvo(1, {'column':'value'}, (err, convo) => {
+      console.log(err)
+    })
+
+  })
+
+  // Landing page w/ `Add to Slack` button
+  server.get('/', (req, res) => {
+
+
+    let redirectURI = `https://${domain || req.get('host')}/add-to-slack`
+
+    res.send(`
+    <html>
+    <body style='display: flex; align-items: center; justify-content: center; flex-direction: column;font-family: sans-serif'>
+      <h1><a href='https://github.com/BeepBoopHQ/slapp'>Slapp</a> Firebase Slack App</h1>
+      <a href="https://slack.com/oauth/authorize?scope=bot&client_id=${clientId}&redirect_uri=${redirectURI}"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+      <a href="https://github.com/selfcontained/slapp-firebase-example"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+    </body>
+    </html>
+    `)
+  })
+
+  // OAuth receiver - exhange, verify and store tokens & team data
+  server.get('/add-to-slack', bodyParser.json(), (req, res) => {
+
+    let params = {
+      code: req.query.code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: `https://${domain || req.get('host')}/add-to-slack`
+    }
+
+    slack.oauth.access(params, (err, oauthAccess) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).send(err.message || err)
+      }
+
+      // test access token
+      slack.auth.test({ token: oauthAccess.access_token }, (err, authTest) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).send(err.message || err)
+        }
+
+        if (!authTest.team_id) {
+          return res.status(500).send('No team_id present in auth.test response')
+        }
+
+        db.saveTeam(authTest.team_id, oauthAccess, (err) => {
+          if (err) {
+            console.error(err)
+            return res.status(500).send(err.message || err)
+          }
+
+          console.log('Team added: %s', authTest.team_id)
+          res.send(`
+            <html>
+            <body style='display: flex; align-items: center; justify-content: center; flex-direction: column;font-family: sans-serif'>
+              <h1><a href='https://github.com/BeepBoopHQ/slapp'>Slapp</a> Firebase Slack App</h1>
+              <p>This Slack App has been successfully added to the <strong>${authTest.team}</strong> team</p>
+              <a href="https://github.com/selfcontained/slapp-firebase-example"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
+            </body>
+            </html>
+          `)
+        })
+      })
+    })
+  })
+}
---
+++ library/slapp.js
@@ -0,0 +1,234 @@
+'use strict'
+
+const Slapp = require('slapp')
+const Context = require('./context')
+const ConvoStore = require('./convo-store')
+
+module.exports = (server, db) => {
+  let app = Slapp({
+    verify_token: process.env.SLACK_VERIFY_TOKEN,
+    context: Context(db),
+    convo_store: ConvoStore(db)
+  })
+
+
+
+  var HELP_TEXT = `
+  I will respond to the following messages:
+  \`help\` - to see this message
+  \`hi\` - to demonstrate a conversation that tracks state.
+  \`thanks\` - to demonstrate a simple response.
+  \`<type-any-other-text>\` - to demonstrate a random emoticon response, some of the time :wink:.
+  \`attachment\` - to see a Slack attachment message.
+  `
+
+//*********************************************
+// Setup different handlers for messages
+//*********************************************
+
+  // response to the user typing "help"
+  app.message('help', ['mention', 'direct_message'], (msg) => {
+    msg.say(HELP_TEXT)
+  })
+
+  // Test response for boxes
+  app.message('yesno', (msg) => {
+      msg.say({
+        text: '',
+        attachments: [
+          {
+            text: '',
+            fallback: 'Yes or No?',
+            callback_id: 'yesno_callback',
+            actions: [
+              { name: 'answer', text: 'Yes', type: 'button', value: 'yes' },
+              { name: 'answer', text: 'No',  type: 'button',  value: 'no' }
+            ]
+          }]
+        })
+  })
+
+  app.action('yesno_callback', 'answer', (msg, value) => {
+    msg.respond(msg.body.response_url, `${value} is a good choice!`)
+  })
+
+  // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
+  app
+    .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
+      msg
+        .say(`${text}, how are you?`)
+        // sends next event from user to this route, passing along state
+        .route('how-are-you', { greeting: text })
+    })
+    .route('how-are-you', (msg, state) => {
+      var text = (msg.body.event && msg.body.event.text) || ''
+
+      // user may not have typed text as their next action, ask again and re-route
+      if (!text) {
+        return msg
+          .say("Whoops, I'm still waiting to hear how you're doing.")
+          .say('How are you?')
+          .route('how-are-you', state)
+      }
+
+      // add their response to state
+      state.status = text
+
+      msg
+        .say(`Ok then. What's your favorite color?`)
+        .route('color', state)
+    })
+    .route('color', (msg, state) => {
+      var text = (msg.body.event && msg.body.event.text) || ''
+
+      // user may not have typed text as their next action, ask again and re-route
+      if (!text) {
+        return msg
+          .say("I'm eagerly awaiting to hear your favorite color.")
+          .route('color', state)
+      }
+
+      // add their response to state
+      state.color = text
+
+      msg
+        .say('Thanks for sharing.')
+        .say(`Here's what you've told me so far: \`\`\`${JSON.stringify(state)}\`\`\``)
+      // At this point, since we don't route anywhere, the "conversation" is over
+    })
+
+  // Can use a regex as well
+  app.message(/^(thanks|thank you)/i, ['mention', 'direct_message'], (msg) => {
+    // You can provide a list of responses, and a random one will be chosen
+    // You can also include slack emoji in your responses
+    msg.say([
+      "You're welcome :smile:",
+      'You bet',
+      ':+1: Of course',
+      'Anytime :sun_with_face: :full_moon_with_face:'
+    ])
+  })
+
+  // demonstrate returning an attachment...
+  app.message('attachment', ['mention', 'direct_message'], (msg) => {
+    msg.say({
+      text: 'Check out this amazing attachment! :confetti_ball: ',
+      attachments: [{
+        text: 'Slapp is a robust open source library that sits on top of the Slack APIs',
+        title: 'Slapp Library - Open Source',
+        image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+        title_link: 'https://beepboophq.com/',
+        color: '#7CD197'
+      }]
+    })
+  })
+
+  // response to the user asking "how did you get here?"
+  app.message('how did you get here?', ['mention', 'direct_message'], (msg) => {
+    msg.say('Team Rocket! :rocket:')
+  })
+
+  // "Creative motivators" flow
+  app
+    .message('motivators', ['direct_mention', 'direct_message'], (msg, text) => {
+      msg
+        .say(`What is something that motivates you to create?`)
+        // sends next event from user to this route, passing along state
+        .route('motivates')
+    })
+    .route('motivates', (msg, state) => {
+      var text = (msg.body.event && msg.body.event.text) || ''
+
+      // user may not have typed text as their next action, ask again and re-route
+      if (!text) {
+        return msg
+          .say("Whoops, I'm still waiting to hear a response.")
+          .say('What is something that motivates you to create?')
+          .route('motivates', state)
+      }
+
+      // add their response to state
+      state.motivates = text
+
+      msg
+        .say(`Ok. And what's something that discourages you from creating?`)
+        .route('discourages', state)
+    })
+    .route('discourages', (msg, state) => {
+      var text = (msg.body.event && msg.body.event.text) || ''
+
+      // user may not have typed text as their next action, ask again and re-route
+      if (!text) {
+        return msg
+          .say("I'm eagerly awaiting to hear something that discourages you from creating.")
+          .route('discourages', state)
+      }
+
+      // add their response to state
+      state.discourages = text
+
+      msg
+        .say('Thanks for sharing.')
+        .say(`Do more of this: `+state.motivates+'. Do less of this: '+state.discourages)
+      // At this point, since we don't route anywhere, the "conversation" is over
+    })
+
+  // Catch-all for any other responses not handled above
+  app.message('.*', ['direct_mention', 'direct_message'], (msg) => {
+    // respond only 40% of the time
+    if (Math.random() < 0.4) {
+      msg.say([':wave:', ':pray:', ':raised_hands:'])
+    }
+  })
+
+
+
+
+
+
+  app
+    .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
+      msg
+        .say(text + ', how are you?')
+        .route('how-are-you', { text }, 60)
+    })
+    .route('how-are-you', (msg, state) => {
+      var resp = msg.body.event && msg.body.event.text
+
+      if (/good/i.test(resp)) {
+        msg
+          .say(['Great! Ready?', ':smile: Are you sure?'])
+          .route('how-are-you', state, 60)
+      } else {
+        msg
+          .say('Me too', () => msg.say(`Thanks for saying ${state.text}`))
+      }
+    })
+    .message('^(thanks|thank you)', ['mention', 'direct_message'], (msg) => {
+      msg.say(["You're welcome", 'Of course'])
+    })
+    .message('help', ['direct_message', 'direct_mention'], (msg) => {
+      let help = `I will respond to the following messages:
+\`@slapp hi||hello\` for a greeting.
+\`mention me by name in a message\` to demonstrate detecting a mention.
+\`@slapp help\` to see this again.`
+
+      msg.say(help)
+    })
+    .message('fix it', 'ambient', (msg) => {
+      msg.say('https://www.youtube.com/watch?v=yo3uxqwTxk0')
+    })
+    .message(/.*/, 'mention', (msg) => {
+      msg.say('You really do care about me. :heart:')
+    })
+    .attachToExpress(server)
+
+
+
+
+
+
+
+
+  return app
+}
