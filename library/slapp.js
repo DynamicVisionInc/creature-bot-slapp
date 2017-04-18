e80b3b5da8ab74227c0674e987cd845d7585afc4
JaronKing <jaronjking@gmail.com>
2017-04-18 15:40:05 -0700

    Replies with an image.

--- library/slapp.js
+++ library/slapp.js
@@ -52,16 +52,31 @@ module.exports = (server, db) => {
     var text = (msg.body.event && msg.body.event.text) || ''
     msg.say({
       text: '',
-      attachments: [
+      "attachments": [
         {
-          title: 'Title',
-          pretext: 'Pretext _supports_ mrkdwn',
-          text: 'Testing *right now!*',
-          mrkdwn_in: [
-            "text",
-            "pretext"
-          ]
-        }]
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment",
+            "fields": [
+                {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                }
+            ],
+            "image_url": "https://media.stsci.edu/uploads/story/thumbnail/1178/low_STSCI-H-p1715a-t-400x400.png",
+            "thumb_url": "https://media.stsci.edu/uploads/story/thumbnail/1178/low_STSCI-H-p1715a-t-400x400.png",
+            "footer": "Slack API",
+            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+            "ts": 123456789
+        }
+      ]
       })
     msg.say(['<https://media.stsci.edu/uploads/story/thumbnail/1178/low_STSCI-H-p1715a-t-400x400.png>'])
   })
