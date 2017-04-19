4a01f554698b9160e06414f3cb311e2cd2e02559
JaronKing <jaronjking@gmail.com>
2017-04-19 10:45:26 -0700

    remove add from space image add.

--- library/slapp.js
+++ library/slapp.js
@@ -81,17 +81,6 @@ module.exports = (server, db) => {
   })
 
   app.message('space', ['direct_mention', 'direct_message'], (msg, text) => {
-     db.saveSpaceImages( {
-      'title': "Core of the Crab Nebula",
-      'text': 'Peering deep into the core of the Crab Nebula, this close-up image reveals the beating heart of one of the most historic and intensively studied remnants of a supernova, an exploding star.',
-      'image': 'https://media.stsci.edu/uploads/image/display_image/3760/low_xlarge_web.jpg',
-    },(err) => {
-        if (err) {
-          console.error(err)
-          msg.say(err)
-        }
-
-    })
     // Pull from database space images and captions
     db.getRandomSpaceImage((err, spaceImage) => {
         if (err) {
@@ -134,36 +123,6 @@ module.exports = (server, db) => {
 
     })
 
-
-    // Setup the attachment for the response
-    msg.say({
-      text: '',
-      "attachments": [
-        {
-            "fallback": "Required plain-text summary of the attachment.",
-            "color": "#36a64f",
-            "pretext": "Optional text that appears above the attachment block",
-            "author_name": "Bobby Tables",
-            "author_link": "http://flickr.com/bobby/",
-            "author_icon": "http://flickr.com/icons/bobby.jpg",
-            "title": "Slack API Documentation",
-            "title_link": "https://api.slack.com/",
-            "text": "Optional text that appears within the attachment",
-            "fields": [
-                {
-                    "title": "Priority",
-                    "value": "High",
-                    "short": false
-                }
-            ],
-            "image_url": "https://media.stsci.edu/uploads/story/thumbnail/1178/low_STSCI-H-p1715a-t-400x400.png",
-            "thumb_url": "https://media.stsci.edu/uploads/story/thumbnail/1178/low_STSCI-H-p1715a-t-400x400.png",
-            "footer": "Slack API",
-            "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-            "ts": 123456789
-        }
-      ]
-    })
   })
 
   app.message('test', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
