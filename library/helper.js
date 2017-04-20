46d9b0363836fe6b9486884b55001dda60f46f7d
JaronKing <jaronjking@gmail.com>
2017-04-20 12:49:55 -0700

    Removes random color code.

--- library/helper.js
+++ library/helper.js
@@ -6,14 +6,14 @@ module.exports = () => {
 	return {
 
 		// Retrieve a random color
-		getRandomColor {
-			var letters = '0123456789ABCDEF'
-			var color = '#'
-			for (var i = 0; i < 6; i++) {
-				color += letters[Math.floor(Math.random() * 16)]
-			}
-			return color
-		}
+		// getRandomColor {
+		// 	var letters = '0123456789ABCDEF'
+		// 	var color = '#'
+		// 	for (var i = 0; i < 6; i++) {
+		// 		color += letters[Math.floor(Math.random() * 16)]
+		// 	}
+		// 	return color
+		// }
 
 	}
 }
