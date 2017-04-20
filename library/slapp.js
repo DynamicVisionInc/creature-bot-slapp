56a4f33a2dda3f8cf6e51d2f3cfc8a430f599deb
JaronKing <jaronjking@gmail.com>
2017-04-20 11:54:11 -0700

    Removes random code for color, should be #.

--- library/slapp.js
+++ library/slapp.js
@@ -52,11 +52,11 @@ module.exports = (server, db) => {
     //
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
+    // for (var i = 0; i < 6; i++ ) {
+    //     color += letters[Math.floor(Math.random() * 16)];
+    // }
     console.log(color)
-    msg.say(color_choosen)
+    msg.say(color)
 
   })
 
