c1626d12a2850583200736d75dc9b899d45aabab
JaronKing <jaronjking@gmail.com>
2017-04-26 09:21:11 -0700

    Seperate more business logic from main function for inspire routes.

--- library/inspire.js
+++ library/inspire.js
@@ -3,59 +3,66 @@
 //*********************************************
 // List of Inspire routes
 //*********************************************
+var default_skills = {
+		'color' : 0,
+		'space' : 0
+	}
 
 function getInspireRoute (db, msg) {
 	var route_choosen
-	// Get object of skills
+	// Get object of skills and select a undone route.
 	db.getInspireSkills(msg.body.event.user, (err, inspire_skills) => {
 		if (err) {
 			console.error(err)
 		}
+		var route_choosen = selectInspireSkills(db, inspire_skills)
+		console.log(route_choosen)
+		msg.route(route_choosen + "_route")
+	})
+}
 
-		var skills = inspire_skills
-		if (!(skills instanceof Object))
+function selectInspireSkills (db, inspire_skills) {
+
+	var skills = inspire_skills
+	if (!(skills instanceof Object))
+	{
+		// If no data exists, create object to store skills
+		skills = default_skills
+	}
+	// Create array of skills not done
+	var skills_choosen = []
+	for (var key in skills)
+	{
+		if (skills.hasOwnProperty(key))
 		{
-			// If no data exists, create object to store skills
-			skills = {
-				'color' : 0,
-				'space' : 0
+			if (skills[key] == 0)
+			{
+				skills_choosen.push(key)
 			}
 		}
-		// Create array of skills not done
-		var skills_choosen = []
+	}
+	// If all skills have been done, loop through skills and set to undone and push all skills to skills_choosen
+	if (skills_choosen.length == 0)
+	{
 		for (var key in skills)
 		{
 			if (skills.hasOwnProperty(key))
 			{
-				if (skills[key] == 0)
-				{
-					skills_choosen.push(key)
-				}
+				skills[key] = 0
+				skills_choosen.push(key)
 			}
 		}
-		// If all skills have been done, loop through skills and set to undone and push all skills to skills_choosen
-		if (skills_choosen.length == 0)
-		{
-			for (var key in skills)
-			{
-				if (skills.hasOwnProperty(key))
-				{
-					skills[key] = 0
-					skills_choosen.push(key)
-				}
-			}
-		}
-		// Select randomly from array of skills not done
-		route_choosen = skills_choosen[Math.floor(Math.random() * skills_choosen.length)]
+	}
+	// Select randomly from array of skills not done
+	route_choosen = skills_choosen[Math.floor(Math.random() * skills_choosen.length)]
 
-		skills[route_choosen] = 1
-		// Store changes made to the skills done
-		db.saveInspireSkills(msg.body.event.user, skills, (err, convo) => {
-			console.log(err)
-		})
-		console.log(route_choosen)
-		msg.route(route_choosen + "_route")
+	skills[route_choosen] = 1
+	// Store changes made to the skills done
+	db.saveInspireSkills(msg.body.event.user, skills, (err, convo) => {
+		console.log(err)
 	})
+
+	return route_choosen
 }
 
 module.exports = {
