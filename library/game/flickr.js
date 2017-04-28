7d694e76f6e3353062f9c287676042a82cc630bd
JaronKing <jaronjking@gmail.com>
2017-04-28 16:02:46 -0700

    Does this work?

--- library/game/flickr.js
+++ library/game/flickr.js
@@ -24,7 +24,7 @@ function DeviantArtResponse (msg) {
 				// console.log(raw_data)
 				ParseString(raw_data, function (err, result) {
 					console.log('Parsed raw_data')
-					console.log(result.feed.entry[0].link)
+					console.log(result.feed.entry[0].link[href])
 					for (var prop in result.feed.entry[0].link)
 					{
 						if (result.feed.entry[0].link.hasOwnProperty(prop))
