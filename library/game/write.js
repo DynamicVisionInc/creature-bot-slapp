cccc9d113df1ef57f8bb2fed82625c884f5c13c0
JaronKing <jaronjking@gmail.com>
2017-05-02 09:45:44 -0700

    Adjusted max words to 25.

--- library/game/write.js
+++ library/game/write.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var max_words = 25
+
 function run (msg) {
 	msg.say('Writing everyday will increase your creativity.  Write a 50 word story and I will keep track of the amount of words you use when you enter the message.')
 		.route('write_response', { count: 0 })
@@ -12,14 +14,14 @@ function response (msg, state) {
 	var count = state.count + message_count
 	state.count = count
 	// If count is less than 50, prompt the user and continue back to response
-	if (count < 50)
+	if (count < max_words)
 	{
-		var words_left = 50 - count
+		var words_left = max_words - count
 		msg.say('Still a little more, ' + words_left + ' words to go.')
 			.route('write_response', state)
 	}
 	// If count is greater than 50, prompt the user and end the game
-	else if (count >= 50)
+	else if (count >= max_words)
 	{
 		msg.say('Excellent, now tell me what you think about this short story.')
 			.route('write_end')
