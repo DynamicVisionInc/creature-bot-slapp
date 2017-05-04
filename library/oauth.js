8c6c82739500e757e96da5b767ddc0afe8ecd0dd
JaronKing <jaronjking@gmail.com>
2017-05-04 14:50:40 -0700

    Misspelling.

--- library/oauth.js
+++ library/oauth.js
@@ -53,7 +53,7 @@ module.exports = (server, db) => {
 
       console.log('api')
       console.log(process.env.SLACK_API_TOKEN)
-      consoel.log('api')
+      console.log('api')
       var message = {
         token: 'xoxp-2918553111-20013110662-179088891029-6249dc86e60c14bf3c220a8f9d8ee248',
         channel: "@C0320RUB4",
